2cc7f396f0b3971d890e12bd7f257695d2e61f16
Pragati Ghosh <pragati25ghosh@gmail.com>
2025-08-09 23:38:05 +0530

    navbar 01.00.00

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,10 +3,12 @@ import Home from './pages/Home.jsx'
 import BuyCredit from './pages/BuyCredit.jsx'
 import Result from './pages/Result.jsx'
 import { Route, Routes,  } from 'react-router-dom'
+import Navbar from './components/Navbar.jsx'
 
 const App = () => {
   return (
-    <div>
+    <div className='px-4 sm:px-10 md:px-14 lg:px-28 min-h-screen bg-gradient-to-b from-teal-50 to-orange-50'>
+      <Navbar />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/buy" element={<BuyCredit />} />
