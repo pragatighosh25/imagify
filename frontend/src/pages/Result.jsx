9e09be9b6e340c55dcf4e4eb7fc32fa838fcf285
Pragati Ghosh <pragati25ghosh@gmail.com>
2025-09-11 23:38:44 +0530

    result page

--- frontend/src/pages/Result.jsx
+++ frontend/src/pages/Result.jsx
@@ -1,25 +1,33 @@
 import React from 'react'
 import { assets } from '../assets/assets'
+import { useState } from 'react'
 
 const Result = () => {
+
+  const [image, setImage] = useState(assets.sample_img_1);
+  const [isImageLoaded, setIsImageLoaded] = useState(false);
+  const [loading, setLoading] = useState(false);
   return (
     <form className='flex flex-col min-h-[90vh] justify-center items-center'>
     <div>
       <div className='relative'>
-        <img src={assets.sample_img_1} alt="" className='max-w-sm rounded' />
+        <img src={image} alt="" className='max-w-sm rounded' />
         <span className='absolute h-1 w-full transition-all duration-[10s] bottom-0 left-0 bg-blue-500'/>
       </div>
-      <p>Loading...</p>
+      <p className={!loading ? 'hidden' : ''}>Loading...</p>
     </div>
-
+{!isImageLoaded && 
     <div className='flex w-full max-w-xl text-white bg-neutral-500 text-sm p-0.5 mt-10 rounded-full'>
       <input type="text" placeholder='Describe what you want to generate' className='flex-1 bg-transparent outline-none ml-8 max-sm:w-20 placeholder-color'/>
       <button className='bg-zinc-900 px-10 sm:px-16 py-3 rounded-full'>Generate</button>
     </div>
+}
+{isImageLoaded &&
     <div className='flex gap-2 flex-wrap justify-center text-white text-sm mt-10 rounded-full p-0.5'>
-      <p className='bg-transparent border border-zinc-900 text-black px-8 py-3 rounded-full cursor-pointer'>Generate Another</p>
-      <a href="" download className='bg-zinc-900 px-10 py-3 rounded-full cursor-pointer'>Download</a>
+      <p onClick={() => setIsImageLoaded(false)} className='bg-transparent border border-zinc-900 text-black px-8 py-3 rounded-full cursor-pointer'>Generate Another</p>
+      <a href={image} download className='bg-zinc-900 px-10 py-3 rounded-full cursor-pointer'>Download</a>
     </div>
+}
 
     </form>
   )
