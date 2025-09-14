105fa9dab0005f56286a4cb19e63c63d25527af4
Pragati Ghosh <pragati25ghosh@gmail.com>
2025-09-14 19:10:44 +0530

    login page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,11 +5,13 @@ import Result from './pages/Result.jsx'
 import { Route, Routes,  } from 'react-router-dom'
 import Navbar from './components/Navbar.jsx'
 import Footer from './components/Footer.jsx'
+import Login from './components/Login.jsx'
 
 const App = () => {
   return (
     <div className='px-4 sm:px-10 md:px-14 lg:px-28 min-h-screen bg-gradient-to-b from-teal-50 to-orange-50'>
       <Navbar />
+    <Login />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/buy" element={<BuyCredit />} />
