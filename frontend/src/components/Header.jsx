1d52a099f801626357414b58b409e5f01c5c1730
Pragati Ghosh <pragati25ghosh@gmail.com>
2025-10-28 23:13:50 +0530

    animation

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,15 +1,29 @@
 import React from 'react'
 import { assets } from '../assets/assets.js'
+import {motion} from 'framer-motion'
 
 const Header = () => {
   return (
-    <div className='flex flex-col justify-center items-center text-center my-20'>
-      <div className='text-stone-500 inline-flex text-center gap-2 bg-white px-6 py-1 rounded-full border border-neutral-500'>
+    <motion.div className='flex flex-col justify-center items-center text-center my-20'
+    initial={{opacity:0.2, y:100}}
+    transition={{duration:1}}
+    whileInView={{opacity:1, y:0}}
+    viewport={{once: true}}
+    >
+      <motion.div className='text-stone-500 inline-flex text-center gap-2 bg-white px-6 py-1 rounded-full border border-neutral-500'
+      initial={{opacity:0, y:-20}}
+      animate={{opacity:1, y:0}}
+      transition={{delay:0.2, duration:0.8}}
+      >
         <p>Best text to image generator</p>
         <img src={assets.star_icon} alt="" />
-      </div>
-      <h1 className='text-4xl max-w-[300px] sm:text-7xl sm:max-w-[590px] mx-auto mt-10 text-center'>Turn text to
-        <span className='text-blue-600'> image</span>, in seconds.</h1>
+      </motion.div>
+      <motion.h1 className='text-4xl max-w-[300px] sm:text-7xl sm:max-w-[590px] mx-auto mt-10 text-center'
+      initial={{opacity:0}}
+      animate={{opacity:1}}
+      transition={{delay:0.4, duration:2}}
+      >Turn text to
+        <span className='text-blue-600'> image</span>, in seconds.</motion.h1>
       <p className='text-center max-w-xl mx-auto mt-5'>Unleash your creativity with our AI-powered tool. Turn your imagination into stunning visuals effortlessly.</p>
 
       <button className='sm:text-lg text-white bg-black w-auto mt-8 px-12 py-2.5 rounded-full items-center gap-2 hover:opacity-80 transition-all duration-300'>Generate Images</button>
@@ -20,7 +34,7 @@ const Header = () => {
         ))}
       </div>
       <p className='mt-2 text-neutral-600 '>Generated images from Imagify</p>
-    </div>
+    </motion.div>
   )
 }
 
