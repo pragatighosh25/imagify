b8e8d7d6cd94a1851fc991bf84229a1ee5b808be
Pragati Ghosh <pragati25ghosh@gmail.com>
2025-11-01 00:19:07 +0530

    user controller

---
+++ backend/controllers/userController.js
@@ -0,0 +1,48 @@
+import userModel from "../models/userModels.js";
+import bcrypt from "bcryptjs";
+import jwt from "jsonwebtoken";
+
+const registerUser = async (req, res) => {
+  try{
+    const { name, email, password } = req.body;
+
+    if(!name || !email || !password){
+      return res.status(400).json({message: "All fields are required", success: false});
+    }
+
+    const salt= await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(password, salt);
+
+    const userData = {
+      name,
+      email,
+      password: hashedPassword
+    };
+
+    const newUser = new userModel(userData);
+    const user= await newUser.save();
+
+    const token = jwt.sign(
+      { id: user._id },
+      process.env.JWT_SECRET
+    );
+    res.status(201).json({ token, success: true , user: { name: user.name} });
+  }catch(err){
+    console.error(err);
+    res.status(500).json({message: err.message, success: false});
+  }
+};
+
+const loginUser= async (req, res)=>{
+  try{
+    const {email, password}= req.body;
+    const user= await userModel.findOne({email})
+  }catch(err){
+
+  }
+}
+
+
+
+
+export { registerUser };
