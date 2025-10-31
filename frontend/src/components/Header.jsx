27de6620d082c7ee9eb410beca045f60d439701b
Pragati Ghosh <pragati25ghosh@gmail.com>
2025-10-31 21:07:28 +0530

    animation to different sections

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,8 +1,21 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import { assets } from '../assets/assets.js'
 import {motion} from 'framer-motion'
+import { useNavigate } from 'react-router-dom'
+import { AppContext } from '../context/AppContext.jsx'
 
 const Header = () => {
+
+  const {user, setShowLogin}= useContext(AppContext)
+  const navigate= useNavigate()
+
+  const onClickHandler=() => {
+    if(user){
+      navigate('/result')
+    }else{
+      setShowLogin(true)
+    }
+  }
   return (
     <motion.div className='flex flex-col justify-center items-center text-center my-20'
     initial={{opacity:0.2, y:100}}
@@ -26,14 +39,34 @@ const Header = () => {
         <span className='text-blue-600'> image</span>, in seconds.</motion.h1>
       <p className='text-center max-w-xl mx-auto mt-5'>Unleash your creativity with our AI-powered tool. Turn your imagination into stunning visuals effortlessly.</p>
 
-      <button className='sm:text-lg text-white bg-black w-auto mt-8 px-12 py-2.5 rounded-full items-center gap-2 hover:opacity-80 transition-all duration-300'>Generate Images</button>
+      <motion.button className='sm:text-lg text-white bg-black w-auto mt-8 px-12 py-2.5 rounded-full items-center gap-2'
+      onClick={onClickHandler}
+      whileHover={{scale: 1.05}}
+      whileTap={{scale: 0.95}}
+      initial={{opacity: 0}}
+      animate={{opacity: 1}}
+      transition={{default:{duration: 0.5}, opacity: {delay: 0.8, duration: 1}}}
+      >
+      Generate Images</motion.button>
 
-      <div className='flex flex-wrap justify-center mt-16 gap-3'>
+      <motion.div
+      initial={{opacity:0}}
+      animate={{opacity:1}}
+      transition={{delay:1, duration:1}}
+      
+      className='flex flex-wrap justify-center mt-16 gap-3'>
         {Array(6).fill('').map((item, index) => (
-          <img key={index} src={index%2===0 ? assets.sample_img_1 : assets.sample_img_2} alt='' width={70} className='rounded hover:scale-105 transition-all duration-300 cursor-pointer max-sm:w-10'  />
+          <motion.img
+          whileHover={{scale: 1.05, duration: 0.1}}
+          key={index} src={index%2===0 ? assets.sample_img_1 : assets.sample_img_2} alt='' width={70} className='rounded hover:scale-105 transition-all duration-300 cursor-pointer max-sm:w-10'  />
         ))}
-      </div>
-      <p className='mt-2 text-neutral-600 '>Generated images from Imagify</p>
+      </motion.div>
+      <motion.p
+      initial={{opacity: 0}}
+      animate={{opacity: 1}}
+      transition={{delay: 1.2, duration: 0.8}}
+      
+      className='mt-2 text-neutral-600 '>Generated images from Imagify</motion.p>
     </motion.div>
   )
 }
--- frontend/src/components/Testimonials.jsx
+++ frontend/src/components/Testimonials.jsx
@@ -1,10 +1,17 @@
 import React from 'react'
 import { testimonialsData } from '../assets/assets.js'
 import { assets } from '../assets/assets'
+import {motion} from 'framer-motion'
 
 const Testimonials = () => {
   return (
-    <div className='flex flex-col items-center justify-center my-20 py-12'>
+    <motion.div
+    initial={{opacity:0.2, y:100}}
+    transition={{duration: 1}}
+    whileInView={{opacity: 1, y:0}}
+    viewport={{once: true}}
+    
+    className='flex flex-col items-center justify-center my-20 py-12'>
       <h1 className='text-3xl sm:text-4xl font-semibold mb-2'>Customer Testimonials</h1>
       <p className='text-gray-500 mb-8'>What our customers are saying</p>
 
@@ -23,7 +30,7 @@ const Testimonials = () => {
           </div>
         ))}
       </div>
-    </div>
+    </motion.div>
   )
 }
 
