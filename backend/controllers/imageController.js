b1455c2a763bd25837263cbc282f0b6c86d5a2db
Pragati Ghosh <pragati25ghosh@gmail.com>
2025-11-01 21:00:15 +0530

    image router

--- backend/controllers/imageController.js
+++ backend/controllers/imageController.js
@@ -1,5 +1,5 @@
 import userModel from "../models/userModels.js";
-
+import FormData from "form-data";
 const generateImage = async (req, res) => {
   try {
     const { prompt, userId } = req.body;
@@ -10,7 +10,29 @@ const generateImage = async (req, res) => {
     if(user.creditBalance === 0 || userModel.creditBalance < 1){
       return  res.status(403).json({ message: "No credits balance", credits: user.creditBalance, success: false });
     }
-    
+    const formData = new FormData();
+    formData.append("prompt", prompt);
+
+    const { data } = await axios.post("https://clipdrop-api.co/cleanup/v1", formData, {
+      headers: {
+        'x-api-key': process.env.CLIPDROP_API,
+      },
+      responseType: 'arraybuffer'
+    });
+
+    const base64Image = Buffer.from(data, 'binary').toString('base64');
+
+    const resultImage = `data:image/png;base64,${base64Image}`;
+
+    if(resultImage){
+    await userModel.findByIdAndUpdate(user._id, {
+      creditBalance: user.creditBalance - 1
+    });
+
+    res.status(200).json({ image: resultImage, credits: user.creditBalance - 1, success: true,message: "Image generated successfully"});
+    }else{
+      res.status(500).json({ message: "Image generation failed", success: false });
+    }
   } catch (error) {
     console.error(error);
     res.status(500).json({ message: error.message, success: false });
