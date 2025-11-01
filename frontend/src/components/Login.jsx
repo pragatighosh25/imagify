528d68fd4881cd0c5f77e05b2a6b2e1ff60b1bba
Pragati Ghosh <pragati25ghosh@gmail.com>
2025-11-01 23:55:17 +0530

    credits and logout

--- frontend/src/components/Login.jsx
+++ frontend/src/components/Login.jsx
@@ -1,90 +1,162 @@
-import React, { useContext, useEffect, useState } from 'react'
-import { assets } from '../assets/assets'
-import { AppContext } from '../context/AppContext.jsx';
-import {motion} from 'framer-motion'
-import axios from 'axios';
-import { toast } from 'react-toastify';
+import React, { useContext, useEffect, useState } from "react";
+import { assets } from "../assets/assets";
+import { AppContext } from "../context/AppContext.jsx";
+import { motion } from "framer-motion";
+import axios from "axios";
+import { toast } from "react-toastify";
 
 const Login = () => {
-  const [state, setState] = useState('Login')
-  const { showLogin, setShowLogin, backendUrl, setToken,setUser } = useContext(AppContext);
-  const [name, setName] = useState('');
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
+  const [state, setState] = useState("Login");
+  const { showLogin, setShowLogin, backendUrl, setToken, setUser } =
+    useContext(AppContext);
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
 
   const onSubmitHandler = async (e) => {
     e.preventDefault();
     try {
-      if(state === 'Login'){
-        const {data}= await axios.post(`${backendUrl}/api/user/login`, {
+      if (state === "Login") {
+        const { data } = await axios.post(`${backendUrl}/api/user/login`, {
           email,
-          password
-        })
-        if(data.success){
+          password,
+        });
+        if (data.success) {
           setToken(data.token);
           setUser(data.user);
-          localStorage.setItem('token', data.token);
+          localStorage.setItem("token", data.token);
           setShowLogin(false);
-        }else{
+        } else {
           toast.error(data.message);
         }
       } else {
-        await axios.post(`${backendUrl}/api/user/register`, {
+        const { data } = await axios.post(`${backendUrl}/api/user/register`, {
           name,
           email,
-          password
+          password,
         });
+        if (data.success) {
+          setToken(data.token);
+          setUser(data.user);
+          localStorage.setItem("token", data.token);
+          setShowLogin(false);
+        } else {
+          toast.error(data.message);
+        }
       }
     } catch (error) {
-      
+      console.error("Login Error:", error.response?.data || error.message);
+      toast.error(
+        error.response?.data?.message || "An error occurred. Please try again."
+      );
     }
-
-  }
+  };
 
   useEffect(() => {
-    document.body.style.overflow = 'hidden';
+    document.body.style.overflow = "hidden";
 
     return () => {
-      document.body.style.overflow = 'auto';
-    }
-  }, [])
+      document.body.style.overflow = "auto";
+    };
+  }, []);
 
   return (
-    <div className='fixed top-0 left-0 right-0 bottom-0 z-10 backdrop-blur-sm bg-black/30 flex justify-center items-center'>
-      <motion.form onSubmit={onSubmitHandler}
-      initial={{opacity:0.2, y:50}}
-      transition={{duration:0.3}}
-      whileInView={{opacity: 1, y:0}}
-      viewport={{once: true}}
-      
-      className='relative bg-white p-10 rounded-xl text-slate-500'>
-        <h1 className='text-center text-2xl text-neutral-700 font-medium'>{state}</h1>
-        {state === 'Login' && <p className='text-sm'>Welcome back! Please sign in to continue</p>}
-        {state === 'Sign Up' && <p className='text-sm'>Welcome! Please create an account to continue</p>}
-        {state !== 'Login' && <div className='border px-6 py-2 flex items-center gap-2 rounded-full mt-5'>
-          <img src={assets.profile_icon} alt="" className='w-6 h-6' />
-          <input onChange={e => setName(e.target.value)} value={name} className='outline-none text-sm' type="text" placeholder='Full Name' required/>
-        </div>}
+    <div className="fixed top-0 left-0 right-0 bottom-0 z-10 backdrop-blur-sm bg-black/30 flex justify-center items-center">
+      <motion.form
+        onSubmit={onSubmitHandler}
+        initial={{ opacity: 0.2, y: 50 }}
+        transition={{ duration: 0.3 }}
+        whileInView={{ opacity: 1, y: 0 }}
+        viewport={{ once: true }}
+        className="relative bg-white p-10 rounded-xl text-slate-500"
+      >
+        <h1 className="text-center text-2xl text-neutral-700 font-medium">
+          {state}
+        </h1>
+        {state === "Login" && (
+          <p className="text-sm">Welcome back! Please sign in to continue</p>
+        )}
+        {state === "Sign Up" && (
+          <p className="text-sm">
+            Welcome! Please create an account to continue
+          </p>
+        )}
+        {state !== "Login" && (
+          <div className="border px-6 py-2 flex items-center gap-2 rounded-full mt-5">
+            <img src={assets.profile_icon} alt="" className="w-6 h-6" />
+            <input
+              onChange={(e) => setName(e.target.value)}
+              value={name}
+              className="outline-none text-sm"
+              type="text"
+              placeholder="Full Name"
+              required
+            />
+          </div>
+        )}
 
-        <div className='border px-6 py-2 flex items-center gap-2 rounded-full mt-4'>
-          <img src={assets.email_icon} alt="" className='w-4 h-4' />
-          <input onChange={e => setEmail(e.target.value)} value={email} className='outline-none text-sm' type="email" placeholder='Email Address' required/>
+        <div className="border px-6 py-2 flex items-center gap-2 rounded-full mt-4">
+          <img src={assets.email_icon} alt="" className="w-4 h-4" />
+          <input
+            onChange={(e) => setEmail(e.target.value)}
+            value={email}
+            className="outline-none text-sm"
+            type="email"
+            placeholder="Email Address"
+            required
+          />
         </div>
 
-        <div className='border px-6 py-2 flex items-center gap-2 rounded-full mt-4'>
-          <img src={assets.lock_icon} alt="" className='w-4 h-4' />
-          <input onChange={e => setPassword(e.target.value)} value={password} className='outline-none text-sm' type="password" placeholder='Password' required/>
+        <div className="border px-6 py-2 flex items-center gap-2 rounded-full mt-4">
+          <img src={assets.lock_icon} alt="" className="w-4 h-4" />
+          <input
+            onChange={(e) => setPassword(e.target.value)}
+            value={password}
+            className="outline-none text-sm"
+            type="password"
+            placeholder="Password"
+            required
+          />
         </div>
-        <p className='text-sm text-blue-600 my-4 cursor-pointer'>Forgot Password?</p>
-        <button className='bg-blue-600 text-white py-2 w-full rounded-full'>{state === 'Login' ? 'Login' : 'Create Account'}</button>
+        <p className="text-sm text-blue-600 my-4 cursor-pointer">
+          Forgot Password?
+        </p>
+        <button className="bg-blue-600 text-white py-2 w-full rounded-full">
+          {state === "Login" ? "Login" : "Create Account"}
+        </button>
 
-        {state === 'Login' && <p className='mt-5 text-center'>Dont have an account? <span className='text-blue-600 cursor-pointer' onClick={()=> setState('Sign Up')}>Sign Up</span></p>}
-        {state === 'Sign Up' && <p className='mt-5 text-center'>Already have an Account? <span className='text-blue-600 cursor-pointer' onClick={()=> setState('Login')}>Login</span></p>}
+        {state === "Login" && (
+          <p className="mt-5 text-center">
+            Dont have an account?{" "}
+            <span
+              className="text-blue-600 cursor-pointer"
+              onClick={() => setState("Sign Up")}
+            >
+              Sign Up
+            </span>
+          </p>
+        )}
+        {state === "Sign Up" && (
+          <p className="mt-5 text-center">
+            Already have an Account?{" "}
+            <span
+              className="text-blue-600 cursor-pointer"
+              onClick={() => setState("Login")}
+            >
+              Login
+            </span>
+          </p>
+        )}
 
-        <img src={assets.cross_icon} alt="" className='absolute top-5 right-5 cursor-pointer' onClick={()=>setShowLogin(false)}/>
+        <img
+          src={assets.cross_icon}
+          alt=""
+          className="absolute top-5 right-5 cursor-pointer"
+          onClick={() => setShowLogin(false)}
+        />
       </motion.form>
     </div>
-  )
-}
+  );
+};
 
-export default Login
+export default Login;
--- frontend/src/context/AppContext.jsx
+++ frontend/src/context/AppContext.jsx
@@ -1,6 +1,9 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { createContext } from 'react'
+import { toast } from 'react-toastify';
 import { useState } from 'react'
+import axios from 'axios';
+
 
 export const AppContext = createContext();
 
@@ -12,7 +15,37 @@ const AppContextProvider = (props) => {
 
   const backendUrl = import.meta.env.VITE_BACKEND_URL;
 
-  const value = { user, setUser, showLogin, setShowLogin, backendUrl, token, setToken, credits, setCredits };
+  const loadCreditData = async () => {
+    try {
+      const {data} = await axios.get(`${backendUrl}/api/user/credits`, {
+        headers: {token}
+      })
+      if(data.success){
+        setCredits(data.credits);
+        setUser(data.user);
+      } else {
+        toast.error(data.message);
+      }
+    } catch (error) {
+      console.log(error);
+      toast.error(error.message);
+    }
+  }
+  const logout = () => {
+    setToken(null);
+    setUser(null);
+    localStorage.removeItem('token');
+  }
+
+  useEffect(() => {
+    if(token){
+      loadCreditData();
+    }else{
+      logout();
+    }
+  }, [token]);
+
+  const value = { user, setUser, showLogin, setShowLogin, backendUrl, token, setToken, credits, setCredits, loadCreditData, logout };
   return (
     <AppContext.Provider value={value}>
       {props.children}
