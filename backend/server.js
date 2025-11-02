1b48c96cda79db3e7a936cc482105139ccde8f7c
Pragati Ghosh <pragati25ghosh@gmail.com>
2025-11-02 15:19:01 +0530

    debug

--- backend/server.js
+++ backend/server.js
@@ -26,7 +26,7 @@ app.use(
   cors({
     origin: allowedOrigins,
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
-    allowedHeaders: ["Content-Type", "Authorization"],
+    allowedHeaders: ["Content-Type", "Authorization", "token"],
     credentials: true,
   })
 );
