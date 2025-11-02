125ef0f73a69bd2268e197c840efd40be12af8a6
Pragati Ghosh <pragati25ghosh@gmail.com>
2025-11-02 12:18:52 +0530

    server.js modifed

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,6 @@
 import express from 'express';
 import cors from 'cors';
 import 'dotenv/config';
-
 import connectDB from './config/mongodb.js';
 import userRouter from './routes/userRouter.js';
 import imageRouter from './routes/imageRouter.js';
@@ -11,29 +10,38 @@ const PORT = process.env.PORT || 3000;
 
 await connectDB();
 
+
 const allowedOrigins = [
-  "https://imagifyfrontend-lovat.vercel.app", 
-  "http://localhost:5173" 
+  "https://imagifyfrontend-lovat.vercel.app",
+  "http://localhost:5173"
 ];
 
-app.use(cors({
-  origin: function (origin, callback) {
-    if (!origin) return callback(null, true);
-    if (allowedOrigins.includes(origin)) {
-      callback(null, true);
-    } else {
-      callback(new Error("Not allowed by CORS"));
-    }
-  },
+
+app.use(
+  cors({
+    origin: allowedOrigins,
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+    credentials: true,
+  })
+);
+
+
+app.options("*", cors({
+  origin: allowedOrigins,
+  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+  allowedHeaders: ["Content-Type", "Authorization"],
   credentials: true,
 }));
+
 app.use(express.json());
 
 
-app.use('/api/user', userRouter);
-app.use('/api/image', imageRouter);
-app.get('/', (req, res) => {
-  res.send('Imagify backend is running');   
+app.use("/api/user", userRouter);
+app.use("/api/image", imageRouter);
+
+app.get("/", (req, res) => {
+  res.send("Imagify backend is running");
 });
 
 app.listen(PORT, () => {
