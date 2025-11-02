95b5afb1b1bd410ba8e113b26a6f624ee4c842fd
Pragati Ghosh <pragati25ghosh@gmail.com>
2025-11-02 12:15:46 +0530

    added frontend link

--- backend/server.js
+++ backend/server.js
@@ -11,7 +11,22 @@ const PORT = process.env.PORT || 3000;
 
 await connectDB();
 
-app.use(cors());
+const allowedOrigins = [
+  "https://imagifyfrontend-lovat.vercel.app", 
+  "http://localhost:5173" 
+];
+
+app.use(cors({
+  origin: function (origin, callback) {
+    if (!origin) return callback(null, true);
+    if (allowedOrigins.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error("Not allowed by CORS"));
+    }
+  },
+  credentials: true,
+}));
 app.use(express.json());
 
 
